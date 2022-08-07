646a45a46d48072cbed1205f96df8f002f8dd4af
yokai <ghostcodedev@gmail.com>
2022-08-07 12:13:51 +0100

    fix typo

--- SQL/creation-of-tables/follows.sql
+++ SQL/creation-of-tables/follows.sql
@@ -4,6 +4,6 @@ CREATE TABLE follows (
     followerId int,
     followingId int,
 
-    FOREIGN KEY (followerId) REFERENCES user(id) ON DELETE CASCADE,
-    FOREIGN KEY (followingId) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY (followerId) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (followingId) REFERENCES users(id) ON DELETE CASCADE
 );
